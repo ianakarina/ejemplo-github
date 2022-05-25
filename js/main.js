59e21bcbb73eab0273f2984e9ce421f9ed446486
ianakarina <ianakarina@gmail.com>
2022-05-25 19:47:44 -0400

    Container con funcionalidad JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo')
+const container = document.querySelector('.container'); //Más rápido que getelementbyId
+
+//Creo elemento html
+const parrafo = document.createElement('p');
+// Creo texto párrafo
+const textParrafo = document.createTextNode('Mi párrafo');
+
+//Añado texto al párrafo
+parrafo.appendChild(textParrafo);
+
+//renderizo en el Dom
+container.append(parrafo);
